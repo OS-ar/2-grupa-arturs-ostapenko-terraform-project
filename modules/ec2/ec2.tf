0b2d2f2a21e6ca0fbf7c627057a76aa286be1cbd
OS-ar <arturs.ostapenko90@gmail.com>
2022-05-12 16:51:16 +0300

    edit user_data in ec2 module

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -3,6 +3,11 @@ resource "aws_instance" "Ubuntu_LTS" {
     subnet_id = pub-subnet.id
     user_data = <<EOF
 #!/bin/bash 
+sudo wget http://nginx.org/keys/nginx_signing.key
+sudo apt-key add nginx_signing.key
+cd /etc/apt
+deb http://nginx.org/packages/ubuntu focal nginx
+deb-src http://nginx.org/packages/ubuntu focal nginx
 apt-get update
 apt-get -y install nginx
 sudo systemctl enable nginx
